04f590d9045b775cadb0420e407923813a17ea37
aziko2712 <azamat.sharifov.93@inbox.ru>
2025-10-19 21:06:19 +0500

    Add basic tasks

--- Program.cs
+++ Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+
+string txt = "teLefOn";
+int x = 13;
+if (x > txt.Length)
+{
+    Console.WriteLine(txt.ToUpper());
+}
+else
+{
+    Console.WriteLine(txt.ToLower());
+}
+
+string txt1 = "komPyuter";
+int x1 = 4;
+if (x1 > txt.Length)
+{
+    Console.WriteLine(txt1.ToUpper());
+}
+else
+{
+    Console.WriteLine(txt1.ToLower());
+}
